0ced8c6ec685131a76f0d4f5ef11ea1ffe0ead19
Sugitha-Nadarajah <sugitha.nadarajah@efrei.net>
2025-10-07 16:43:25 +0200

    add kafka file (+ producers / consumers)

---
+++ kafka/consumer_weather.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    "weather_paris",
+    bootstrap_servers="localhost:9092",
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    auto_offset_reset="latest"
+)
+
+print("📡 En attente de messages météo...")
+
+for message in consumer:
+    print("🌦️ Reçu :", message.value)
---
+++ kafka/producer_weather.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer
+import requests, json, time
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+URL = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&current_weather=true"
+
+while True:
+    data = requests.get(URL).json()
+    weather = data["current_weather"]
+    producer.send("weather_paris", weather)
+    print("✅ Message envoyé :", weather)
+    time.sleep(1800)
